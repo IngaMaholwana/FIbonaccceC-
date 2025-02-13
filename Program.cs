b3b154988e02222eb449ff069352b9682cf13469
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-02-13 12:58:13 +0200

    logging and tracing

--- Program.cs
+++ Program.cs
@@ -1,18 +1,30 @@
-﻿int result = Fibonacci(5);
-Console.WriteLine(result);
+﻿using System;
+using System.Diagnostics;
 
-static int Fibonacci(int n)
+class Program
 {
-    int n1 = 0;
-    int n2 = 1;
-    int sum;
-
-    for (int i = 2; i <= n; i++)
+    static void Main()
     {
-        sum = n1 + n2;
-        n1 = n2;
-        n2 = sum;
+        int n = 5;
+        int result = Fibonacci(n);
+        Console.WriteLine(result);
     }
 
-    return n == 0 ? n1 : n2;
+    static int Fibonacci(int n)
+    {
+        Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+        Debug.WriteLine($"We are looking for the {n}th number");
+
+        int n1 = 0, n2 = 1, sum;
+
+        for (int i = 2; i <= n; i++)
+        {
+            sum = n1 + n2;
+            n1 = n2;
+            n2 = sum;
+            Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");
+        }
+
+        return n == 0 ? n1 : n2;
+    }
 }
